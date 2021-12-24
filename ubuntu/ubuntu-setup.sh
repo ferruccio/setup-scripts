6f57156d407abb73d9bbf99c58b4bb92296392df
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2021-12-23 20:21:29 -0500

    Merge dev-tools.sh into ubuntu-setup.sh

--- ubuntu/ubuntu-setup.sh
+++ ubuntu/ubuntu-setup.sh
@@ -8,18 +8,18 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
 
-# install Deno
-curl -fsSL https://deno.land/x/install/install.sh | sh
-cat >>~/.profile <<EOF
-# add deno to path
-export DENO_INSTALL="/home/fgb/.deno"
-export PATH="\$DENO_INSTALL/bin:\$PATH"
-EOF
-
 # install dependencies and tools
 sudo apt install -y libssl-dev subversion htop docker.io
 
 sudo usermod -aG docker $USER
+
+# install AWS CLI
+curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
+unzip awscliv2.zip
+sudo ./aws/install
+
+# install nvm
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
 # install Rust, Cargo plugins & tools
 curl --proto '=https' --tlsv1.2 -sSf -q https://sh.rustup.rs | sh -s -- -y
@@ -27,13 +27,9 @@ source $HOME/.cargo/env
 
 cargo install --target-dir=/tmp/target starship just mcfly
 
-# install nvm
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-
-# install AWS CLI
-curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
-unzip awscliv2.zip
-sudo ./aws/install
+cargo install --target-dir=/tmp/target \
+    cargo-audit cargo-edit cargo-outdated cargo-deny cargo-update \
+    ripgrep exa bat fd-find git-delta mdbook onefetch
 
 # build fast from source
 mkdir ~/bin
@@ -47,9 +43,22 @@ popd
 starship completions bash >~/bin/starship-completions.sh
 just --completions bash >~/bin/just-completions.sh
 
-# configure AWS
-aws configure
-fast ~/.aws/credentials git
+# install .NET
+wget https://dot.net/v1/dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 6.0
+cat >>~/.profile <<EOF
+# add .NET to path
+export PATH="\$HOME/.dotnet:\$PATH"
+EOF
+
+# install Deno
+curl -fsSL https://deno.land/x/install/install.sh | sh
+cat >>~/.profile <<EOF
+# add deno to path
+export DENO_INSTALL="/home/fgb/.deno"
+export PATH="\$DENO_INSTALL/bin:\$PATH"
+EOF
 
 popd
 
@@ -64,3 +73,17 @@ cat .bash_aliases >~/.bash_aliases
 
 # create .config/starship.toml
 cat starship.toml >~/.config/starship.toml
+
+cat <<EOF
+
+To complete setup:
+
+    * reboot system
+
+    * run after login:
+        aws configure
+        nvm install 14
+        nvm install-latest-npm
+        npm install -g aws-cdk
+
+EOF
